281119f834eeb509e893907073f66f0a0f7b16d8
Kimberly Aguilar <757379@student.dusd.net>
2024-04-29 15:28:27 -0700

    started text
    added game over and score text

--- Unit5UserInterfaceAguilar/Assets/Course Library/Scripts/GameManager.cs
+++ Unit5UserInterfaceAguilar/Assets/Course Library/Scripts/GameManager.cs
@@ -1,17 +1,30 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
-    
+    private int score;
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0); 
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
     }
 
     // Update is called once per frame
@@ -21,11 +34,16 @@ public class GameManager : MonoBehaviour
     }
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+    public void UpdateScore(int scoreToAdd)        
+    {
+        score += scoreToAdd;
+        ScoreText.text = "Score: " + score;
     }
 }
